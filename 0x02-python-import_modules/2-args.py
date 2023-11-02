b735f0f3fd749266998b8f671ee2c9eb6ad7b886
oovaa <omarvx211@gmail.com>
2023-11-02 14:55:48 +0200

    fixed 2 at 02-14

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,5 +4,11 @@ import sys
 
 ls = sys.argv[1:]
 
+if len(ls) == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(len(ls)))
+
+
 for i, v in enumerate(ls):
     print("{}: {}".format(i + 1, v))
