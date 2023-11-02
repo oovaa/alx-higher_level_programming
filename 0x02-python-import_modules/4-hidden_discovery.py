15d22832c2fae26ebf3383fb1d7e5658517440c7
oovaa <omarvx211@gmail.com>
2023-11-02 15:15:47 +0200

    4 at 02-15

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
 
-with open('hidden_4.pyc', 'r') as file:
-    content = file.read()
-    print(content)
+    import hidden_4
+
+    if __name__ == "__main__":
+        for name in dir(hidden_4):
+            if not name.startswith("__"):
+                print(name)
