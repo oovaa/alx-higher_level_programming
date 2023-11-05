3886deafa9e14fff9461b440f96dc06543d5d289
oovaa <omarvx211@gmail.com>
2023-11-05 09:21:36 +0200

    hope it checks at 05-09

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,72 +1,43 @@
 #include "lists.h"
 
 /**
- * reverse_listint - reverses a linked list
- * @head: pointer to the first node in the list
- *
- * Return: pointer to the first node in the new list
- */
-void reverse_listint(listint_t **head)
-{
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next = NULL;
-
-	while (current)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-
-	*head = prev;
-}
-
-/**
  * is_palindrome - checks if a linked list is a palindrome
  * @head: double pointer to the linked list
  *
  * Return: 1 if it is, 0 if not
  */
+
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
+	listint_t *last = *head, *start = *head;
+	int len = 0;
+	int mid, i, j;
 
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	while (1)
+	while (last)
 	{
-		fast = fast->next->next;
-		if (!fast)
-		{
-			dup = slow->next;
-			break;
-		}
-		if (!fast->next)
-		{
-			dup = slow->next->next;
-			break;
-		}
-		slow = slow->next;
+		last = last->next;
+		len++;
+	}
+	mid = len / 2;
+
+	int arr[len];
+
+	for (i = 0; start; i++)
+	{
+		arr[i] = start->n;
+		start = start->next;
 	}
 
-	reverse_listint(&dup);
+	j = len - 1;
 
-	while (dup && temp)
+	for (i = 0; i < mid; i++, j--)
 	{
-		if (temp->n == dup->n)
-		{
-			dup = dup->next;
-			temp = temp->next;
-		}
-		else
+		if (arr[i] != arr[j])
 			return (0);
 	}
 
-	if (!dup)
-		return (1);
-
-	return (0);
+	return (1);
 }
