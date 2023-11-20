f54b363745f052fa680949b51b1beba40964062f
oovaa <omarvx211@gmail.com>
2023-11-20 06:04:22 +0200

    project 5 mandatory at 20-06

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    c = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]),end="")
+            c += 1
+        except:
+            break
+    print()
+    
+    return c
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    newlist = []
+    for i in range(list_length):
+        try:
+            newlist.append(my_list_1[i] / my_list_2[i])
+
+        except ZeroDivisionError:
+            print("division by 0")
+            newlist.append(0)
+        except TypeError:
+            print("wrong type")
+            newlist.append(0)
+        except:
+            newlist.append(0)
+
+    return newlist
