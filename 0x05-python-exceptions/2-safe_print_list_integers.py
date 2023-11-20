3baa5bcaac6bfd567a86538387c890c9d4a47d6a
oovaa <omarvx211@gmail.com>
2023-11-20 11:38:23 +0200

    add         except (ValueError, TypeError):  at 20-11

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     c = 0
-    for i in range(x):
+    for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
             c += 1
-        except Exception:
+        except (ValueError, TypeError):
             continue
     print()
 
