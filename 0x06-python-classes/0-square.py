9f2663fd681c8fbdf47c55af73a988d24796b06f
oovaa <omarvx211@gmail.com>
2023-11-21 13:07:56 +0200

    0.1 at 21-13

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -15,12 +15,4 @@ class Square:
     Methods:
         __init__(self, size): Initializes a new instance of the Square class.
     """
-
-    def __init__(self, size):
-        """
-        Initializes a new instance of the Square class.
-
-        Args:
-            size (int): The size of the square.
-        """
-        self.__size = size
+    pass
