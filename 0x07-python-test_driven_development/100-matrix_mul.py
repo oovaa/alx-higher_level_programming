d123637c7eb82cb9d9034f1fbef0c112fa0202d2
oovaa <omarvx211@gmail.com>
2023-11-29 22:18:19 +0200

    pcs at 29-22

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
+"""Defines a matrix multiplication function."""
+
 
 def matrix_mul(m_a, m_b):
+    """Multiply two matrices.
+
+    Args:
+        m_a (list of lists of ints/floats): The first matrix.
+        m_b (list of lists of ints/floats): The second matrix.
+    Raises:
+        TypeError: If either m_a or m_b is not a list of lists of ints/floats.
+        TypeError: If either m_a or m_b is empty.
+        TypeError: If either m_a or m_b has different-sized rows.
+        ValueError: If m_a and m_b cannot be multiplied.
+    Returns:
+        A new matrix representing the multiplication of m_a by m_b.
+    """
 
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
