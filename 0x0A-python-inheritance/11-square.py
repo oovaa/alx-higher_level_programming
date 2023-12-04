77b6ba3ebdfd4a012e8d6af2e2862b1c723c197d
oovaa <omarvx211@gmail.com>
2023-12-04 14:13:06 +0200

    test3 at 04-14

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -3,38 +3,18 @@
 
 Rectangle = __import__('9-rectangle').Rectangle
 
-
 class Square(Rectangle):
-    """Represent a square."""
-
+    """A representation of a square"""
     def __init__(self, size):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square.
-        """
+        """instantiation of the square"""
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
-        """
-        Calculate the area of the Square.
-
-        Returns:
-            int: The area of the Square.
-        """
-
-        return self.__size * self.__size
+        """"returns the area of the square"""
+        return self.__size ** 2
 
     def __str__(self):
-        return "[Square] {}/{}".format(self.__size, self.__size)
-
-    def print(self):
-        """
-        Print the string representation of the Square.
-
-        This method prints the square's details. It's an additional method for
-        and utilizes the __str__ method for its output.
-        """
-        print(self.__str__())
+        """informal string reepresentation of the square"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
