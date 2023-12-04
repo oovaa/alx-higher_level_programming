ef1be63d6abb4a978c461840df1962b7239ddaf0
oovaa <omarvx211@gmail.com>
2023-12-04 14:13:42 +0200

    last fix at 04-14

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -3,8 +3,10 @@
 
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """A representation of a square"""
+
     def __init__(self, size):
         """instantiation of the square"""
         self.integer_validator("size", size)
