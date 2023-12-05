ad2a087e64c00a9738ed666c4f7eb40d05ca2049
oovaa <omarvx211@gmail.com>
2023-12-05 15:26:11 +0200

    at 05-15

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,21 +1,46 @@
 #!/usr/bin/python3
 
-"""the sitty modual"""
+"""The Sitty Module.
 
+This module defines the Student class,
+which models a basic student with
+attributes for first name, last name,
+and age. It provides functionality
+to represent the student's information in JSON format.
+"""
 
 class Student:
-    first_name = None
-    last_name = None
-    age = None
+    """A class representing a student.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
 
     def __init__(self, first_name, last_name, age):
+        """Initialize a new Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        result = dict()
-        result["first_name"] = self.first_name
-        result["last_name"] = self.last_name
-        result["age"] = self.age
+        """Convert the student's attributes to a dictionary.
+
+    This method creates and returns a dictionary representation of the student,
+    with keys for 'first_name', 'last_name', and 'age'.
+    Returns:
+            dict: A dictionary containing the student's attributes.
+        """
+        result = {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age
+        }
         return result
