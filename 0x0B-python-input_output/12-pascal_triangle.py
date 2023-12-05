1bfde531552ad9d36dfb793ff519bbf484375c7f
oovaa <omarvx211@gmail.com>
2023-12-05 16:10:00 +0200

    12 at 05-16

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""Task 12: Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """Create a Pascal's Triangle.
+
+    Args:
+        n (int): The number of rows of the triangle to generate.
+
+    Returns:
+        list: A list of lists, where each inner list represents a row of Pascal's Triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # The first row is always [1]
+
+    for i in range(1, n):
+        row = [1]  # Every row starts with 1
+        for j in range(1, i):
+            # Add the sum of the two numbers above this position.
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)  # Every row ends with 1
+        triangle.append(row)
+
+    return triangle
+
