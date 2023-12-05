ce23e3dd799861d07e2e5ad93b0de7d37995b3bb
oovaa <omarvx211@gmail.com>
2023-12-05 22:14:06 +0200

    last task at 05-22

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import sys
+
+
+def print_stats(total_size, status_codes):
+    """
+    Print the statistics of log processing.
+
+    Args:
+        total_size (int): The total size of files processed.
+        status_codes (dict): A dictionary mapping status codes
+        to their occurrence count.
+    """
+    print("File size: {}".format(total_size))
+    for status in sorted(status_codes.keys()):
+        print("{}: {}".format(status, status_codes[status]))
+
+
+try:
+    codes = {}  # Dictionary to store status codes and their counts
+    tot = 0  # Variable to store total file size
+    roal = 0  # Variable to count the number of lines processed
+
+    for line in sys.stdin:
+        line = line.split()
+
+        # Extract status code and file size from the line
+        stat = line[-2]
+        num = int(line[-1])
+
+        # Update total file size
+        tot += num
+
+        # Update status code count
+        codes[stat] = codes.get(stat, 0) + 1
+
+        # Increment the line count and print stats every 10 lines
+        roal += 1
+        if roal % 10 == 0:
+            print_stats(tot, codes)
+
+except KeyboardInterrupt:
+    # Print stats upon keyboard interruption
+    print_stats(tot, codes)
+    raise
