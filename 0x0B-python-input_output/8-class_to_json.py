3bd7ec1193b32ab4b9451da01e6697b8782f5ced
oovaa <omarvx211@gmail.com>
2023-12-05 15:05:10 +0200

    8 at 05-15

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
+"""class to json modual"""
+
 
 import json
 
+
 def class_to_json(obj):
-    return json.dump(obj)
+    """
+    Convert the attributes of an object into a JSON string.
+
+This function takes an object, accesses its `__dict__` attribute, and then
+serializes that dictionary to a JSON-formatted string using `json.dumps`.
+The `__dict__` attribute of the object contains all its instance attributes,
+which this function will serialize. It's important to ensure that all the
+attributes are serializable to JSON (e.g., they should be of types like
+dict, list, str, int, float, bool, or None).
+
+    Args:
+obj (object): The object whose attributes are to be serialized.
+
+    Returns:
+str: A JSON string representing the serialized attributes of the object.
+    """
+    return json.dumps(obj.__dict__)
