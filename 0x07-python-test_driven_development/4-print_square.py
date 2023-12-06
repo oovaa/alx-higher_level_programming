2a6561cab6f58c276e06a7e2aa1641f19143c626
oovaa <omarvx211@gmail.com>
2023-12-06 13:06:00 +0200

    handle 0 at 06-13

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -9,7 +9,7 @@ def print_square(size):
 
     if not isinstance(size, (int, float)):
         raise TypeError("size must be an integer")
-    if size <= 0:
+    if size < 0:
         raise TypeError("size must be >= 0")
 
     for x in range(size):
