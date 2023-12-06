4c9355c804dbbc2833bf0d1af568dac4b48d4321
oovaa <omarvx211@gmail.com>
2023-12-06 13:16:46 +0200

    last try at 06-13

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""Defines a square-printing function."""
+"""dum docs
+task 4
+"""
 
 
 def print_square(size):
-    """Print a square with the # character.
+    """the dum function"""
 
-    Args:
-        size (int): The height/width of the square.
-    Raises:
-        TypeError: If size is not an integer.
-        ValueError: If size is < 0
-    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError("size must be >= 0")
+        raise TypeError("size must be >= 0")
 
-    for i in range(size):
-        [print("#", end="") for j in range(size)]
-        print("")
+    for x in range(size):
+        print("#" * size)
