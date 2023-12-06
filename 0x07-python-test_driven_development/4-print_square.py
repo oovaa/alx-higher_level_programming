5937066d6bc0c3a3091957462b44b55aeed0471e
oovaa <omarvx211@gmail.com>
2023-12-06 12:52:29 +0200

    at 06-12

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,27 +1,6 @@
 #!/usr/bin/python3
-"""dum docs"""
-
 
 def print_square(size):
-    """
-    Print a square of a given size using the '#' character.
-
-    Parameters:
-    size (int): The size of the sides of the square. Must be a non-negative integer.
-
-    Raises:
-    TypeError: If 'size' is not an integer.
-    ValueError: If 'size' is less than or equal to 0.
-    """
-
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-
-    if size <= 0:
-        raise ValueError("size must be > 0")
-
-    for x in range(size):
-        print("#" * size)
 
     if not isinstance(size, (int, float)):
         raise TypeError("size must be an integer")
