3cb547d49497ab1ab7ec38af88a56e9c69e235ac
oovaa <omarvx211@gmail.com>
2023-12-07 08:25:32 +0200

    till 11 at 07-08

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,6 +4,44 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """
+    Square class that inherits from Rectangle.
+
+    Since a square is a rectangle with equal width and height, this class
+    uses the same attributes as Rectangle.
+    """
 
     def __init__(self, size, x=0, y=0, id=None):
-        super.__init__(id, x, y)
+        """
+        Initialize a new Square instance.
+
+        Args:
+            size (int): The size of the square's sides.
+            x (int, optional): The x-coordinate of the square. Defaults to 0.
+            y (int, optional): The y-coordinate of the square. Defaults to 0.
+            id (int, optional): The id of the square. Defaults to None.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Return a string representation of the square.
+
+        Returns:
+            str: A string representation of the square.
+        """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("width must be an integer")
+
+        if size <= 0:
+            raise ValueError("width must be > 0")
+        self.width = size
+        self.height = size
