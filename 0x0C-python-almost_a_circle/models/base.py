1b29046e4ca93d9023723ae98949e5cc65cdc7c5
oovaa <omarvx211@gmail.com>
2023-12-07 20:28:15 +0200

    task 16 at 07-20

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,3 +14,18 @@ class Base:
 
     def to_json_string(list_dictionaries):
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Writes the JSON string representation of list_objs to a file.
+
+        Args:
+            list_objs (list): A list of instances that inherit from Base.
+        """
+        list_dictionaries = [obj.to_dictionary() for obj in list_objs] if list_objs else []
+        json_string = json.dumps(list_dictionaries)
+
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as f:
+            f.write(json_string)
