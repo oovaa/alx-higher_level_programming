9c5dc16f0d0feac23216a1fa2b8f291c8699fe96
oovaa <omarvx211@gmail.com>
2024-01-17 12:19:23 +0200

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,8 @@
--- Write a script that lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
+-- Write a script that lists all cities contained in the database hbtn_0d_usa.
+
+USE hbtn_0d_usa;
+
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
