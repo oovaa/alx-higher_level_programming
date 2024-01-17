fef529106f044e88ee79be50d49cc106cdf0a71c
oovaa <omarvx211@gmail.com>
2024-01-17 11:43:46 +0200

    fix 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,7 +1,3 @@
--- Filename: list_user_privileges.sql
-
--- List privileges for user_0d_1
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- List privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
