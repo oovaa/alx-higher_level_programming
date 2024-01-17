d228e6dac93b4db46245601785f0243edc1ecf85
oovaa <omarvx211@gmail.com>
2024-01-17 11:53:35 +0200

    typo

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,7 +6,6 @@ USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
-    state_id INT NOT NULLZ ,
+    state_id INT NOT NULL ,
     name VARCHAR(256) NOT NULL
-    FOREIGN KEY (state_id) REFERENCES states(id)
 );
