c1b12a9d886091c06de13dc0b66d7ac209fc0f4a
oovaa <omarvx211@gmail.com>
2024-02-16 22:35:23 +0200

    fuckin chmod at 16-22

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
+
+"""a script that prints the count of State objects
+from the database hbtn_0e_6_usa with a specific name
 """
+
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
 if __name__ == "__main__":
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).filter(State.name == (sys.argv[4],))
-    try:
-        print(instance[0].id)
-    except IndexError:
+
+    state = session.query(State).filter(State.name == (sys.argv[4],))[0]
+
+    if state:
+        print(state.id)
+    else:
         print("Not found")
+
+    session.close()
