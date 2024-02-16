4725946754c816f4a22efd803d476394a126a128
oovaa <omarvx211@gmail.com>
2024-02-16 10:03:16 +0200

    pcs task 2 at 16-10

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""a script that takes in an argument and displays 
-all values in the states table of hbtn_0e_0_usa 
+"""a script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
 where name matches the argument.
 """
 import sys
 import MySQLdb
+
 
 if __name__ == "__main__":
 
@@ -14,7 +15,12 @@ if __name__ == "__main__":
 
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(to_search))
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+            to_search))
     rows = cur.fetchall()
     for r in rows:
         print(r)
+
+    # Close the connection
+    cur.close()
+    db.close()
