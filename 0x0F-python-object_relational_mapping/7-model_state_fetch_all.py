4b75780e432562d00d5dd8a7fb4396477e468b15
oovaa <omarvx211@gmail.com>
2024-02-16 14:30:07 +0200

    task 7 wont run at 16-14

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,9 +4,23 @@
 objects from the database hbtn_0e_6_usa
 """
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from sys import argv
 
 if __name__ == "__main__":
     connection_string = "mysql+mysqlconnector://{}:{}@localhost:3306/{}".format(
         argv[1], argv[2], argv[3])
+
+    engine = create_engine(connection_string)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    table_data = session.query(State).all()
+
+    for state in table_data:
+        print(f"ID: {state.id}, Name: {state.name}")
+
+    session.close()
