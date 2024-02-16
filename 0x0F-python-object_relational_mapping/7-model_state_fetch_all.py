48c1423c8bffdb75b8739d768dd383d66d4f186f
oovaa <omarvx211@gmail.com>
2024-02-16 14:31:02 +0200

    fix pcs at 16-14

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,8 +9,8 @@ from model_state import Base, State
 from sys import argv
 
 if __name__ == "__main__":
-    connection_string = "mysql+mysqlconnector://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3])
+    connection_string = "mysql+mysqlconnector://{}:{}@localhost:3306/{}".
+    format(argv[1], argv[2], argv[3])
 
     engine = create_engine(connection_string)
 
