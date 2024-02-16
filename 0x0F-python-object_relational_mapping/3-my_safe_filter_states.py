c33c788386ea4341157dc802fa3561409489af2d
oovaa <omarvx211@gmail.com>
2024-02-16 10:08:54 +0200

    task 3 at 16-10

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
-"""a script that takes in an argument and displays 
-all values in the states table of hbtn_0e_0_usa 
-where name matches the argument.
-"""
-import sys
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-
-    searched = sys.argv[4]
-    print(searched)
-
-    if "'" in searched:
-        searched = searched[:searched.index("'")]
-
-    print(searched)
-
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-
     cur = db.cursor()
-
-    # Use parameterized query
-    query = "SELECT * FROM states WHERE name = %s"
-    cur.execute(query, (searched,))
-
+    match = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
     rows = cur.fetchall()
-    for r in rows:
-        print(r)
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
