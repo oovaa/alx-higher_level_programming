38080bfe69ed4d9cadcfaa6d3df894c64bb1f739
oovaa <omarvx211@gmail.com>
2024-02-17 18:53:11 +0200

    task 10.2313141 at 17-18

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.name == (sys.argv[4],)).first()
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
 
     if state:
         print(state[0].id)
