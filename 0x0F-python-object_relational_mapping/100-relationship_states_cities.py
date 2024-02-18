f7d02609d48f8465071f5d94ee10f5429813737b
oovaa <omarvx211@gmail.com>
2024-02-18 09:14:21 +0200

    task 15.2 at 18-09

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -12,23 +12,32 @@ from model_city import City
 
 if __name__ == "__main__":
 
+    # Connect to the database engine
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+
+    # Create the database tables
     Base.metadata.create_all(engine)
+
+    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    # Define state and city names
     state_name = "California"
     city_name = "San Francisco"
 
-    new_state = State()
-    new_city = City()
+    # Create instances of State and City
+    new_state = State(name=state_name)
+    new_city = City(name=city_name)
 
-    new_city.name = city_name
-    new_state.name = state_name
-
+    # Establish the relationship by appending the city to the state's cities list
     new_state.cities.append(new_city)
 
+    # Add the state to the session and commit changes
     session.add(new_state)
     session.commit()
+
+    # Close the session
     session.close()
