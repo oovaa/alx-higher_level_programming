940bef5a114cd047cd4f0bf9fde27b42662c3519
oovaa <omarvx211@gmail.com>
2024-03-01 14:21:15 +0200

    task 6.2 at 01-14

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-# Write a Python script that fetches
+"""# Write a Python script that fetches
 # https://alx-intranet.hbtn.io/status
+"""
+
 import requests
 import sys
 if __name__ == '__main__':
