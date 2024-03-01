1f4285fcb764eea206b52a4c23f3d0ae25e74e51
oovaa <omarvx211@gmail.com>
2024-03-01 15:01:12 +0200

    pcs at 01-15

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Write a Python script that takes your GitHub credentials 
+"""Write a Python script that takes your GitHub credentials
 (username and password) and uses the GitHub API to display your id
 """
 import sys
