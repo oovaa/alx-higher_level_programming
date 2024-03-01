9a1311fe69a479d884d40b9271b2233a2723b48c
oovaa <omarvx211@gmail.com>
2024-03-01 19:23:45 +0200

    task 10 Time for an interview! at 01-19

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Please list 10 commits (from the most recent to oldest) of the repository “rails” by the user “rails”
+You must use the GitHub API, here is the documentation https://developer.github.com/v3/repos/commits/
+Print all commits by: `<sha>: <author name>` (one by line)
+
+Write a Python script that takes 2 arguments
+in order to solve this challenge.
+"""
+
+import sys
+import requests
+
+if __name__ == '__main__':
+    if __name__ == '__main__':
+        if len(sys.argv) != 3:
+            print("Usage: python script.py <user> <repository>")
+            sys.exit(1)
+
+    user = sys.argv[1]
+    repos = sys.argv[2]
+    url = f'https://api.github.com/repos/{user}/{repos}/commits'
+
+    response = requests.get(url)
+    response.raise_for_status()
+    commits = response.json()
+
+    for commit in commits[:10]:
+        name = commit.get('commit').get('author').get('name')
+        sha = (commit.get('sha'))
+        print(f"{sha}: {name}")
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,10 +2,9 @@
 """# Write a Python script that fetches
 # https://alx-intranet.hbtn.io/status"""
 
+
 if __name__ == '__main__':
     import requests
     import sys
-
-if __name__ == '__main__':
     re = requests.get(sys.argv[1])
     print(re.headers.get('X-Request-Id'))
