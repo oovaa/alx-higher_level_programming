00b19cabbcfe12467a0bd56716f5075547701d79
oovaa <omarvx211@gmail.com>
2024-03-01 14:12:21 +0200

    task 0 string docs and if at the start at 01-14

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-import urllib.request
-
-url = 'https://alx-intranet.hbtn.io/status'
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
 
 if __name__ == '__main__':
-    with urllib.request.urlopen(url) as response:
-        body_content = response.read()
-        utf8_content = body_content.decode('utf-8')
+    import urllib.request
 
-        print(f"\t- type: {type(body_content)}")
-        print(f"\t- content: {body_content}")
-        print(f"\t- utf8 content: {utf8_content}")
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
