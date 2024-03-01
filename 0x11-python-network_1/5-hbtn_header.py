4d393947dff00d198f3ddded6ce829deda080128
oovaa <omarvx211@gmail.com>
2024-03-01 14:19:20 +0200

    task 5.2 at 01-14

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-# Write a Python script that fetches
-# https://alx-intranet.hbtn.io/status
-import requests
-import sys
+"""# Write a Python script that fetches
+# https://alx-intranet.hbtn.io/status"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
 
 if __name__ == '__main__':
     re = requests.get(sys.argv[1])
