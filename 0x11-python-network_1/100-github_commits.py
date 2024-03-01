7c3fe25643eec474a2ac499324f598946d2845a5
oovaa <omarvx211@gmail.com>
2024-03-01 19:25:43 +0200

    pcs.2 at 01-19

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-Please list 10 commits (from the most recent to oldest) of the repository 
+Please list 10 commits (from the most recent to oldest) of the repository
 “rails” by the user “rails”
-You must use the GitHub API, here is the documentation 
+You must use the GitHub API, here is the documentation
 https://developer.github.com/v3/repos/commits/
 Print all commits by: `<sha>: <author name>` (one by line)
 
