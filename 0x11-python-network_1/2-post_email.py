6f81f31b9150ddab5ffc6543c43020750066419c
oovaa <omarvx211@gmail.com>
2024-03-01 14:16:05 +0200

    task 2.2 at 01-14

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-# Write a Python script that takes in a URL and an email,
+"""# Write a Python script that takes in a URL and an email,
 # sends a POST request to the passed URL with the email as a parameter,
 # and displays the body of the response (decoded in utf-8)
-
-import urllib.request
-import sys
-import urllib.parse
+"""
 
 if __name__ == '__main__':
+    import urllib.request
+    import sys
+    import urllib.parse
+
     url = sys.argv[1]
     email = {"email": sys.argv[2]}
 
