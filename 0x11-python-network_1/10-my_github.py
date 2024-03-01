a2c660010c00d5fdbf74f375a213020e80990fd0
oovaa <omarvx211@gmail.com>
2024-03-01 14:58:22 +0200

    task 10 at 01-14

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,3 +2,18 @@
 """Write a Python script that takes your GitHub credentials 
 (username and password) and uses the GitHub API to display your id
 """
+from sys import argv
+import requests
+
+if __name__ == '__main__':
+
+    user = argv[1]
+    tok = argv[2]
+
+    url = f'https://api.github.com/users/{user}'
+
+    # Make a GET request to the GitHub API
+    response = requests.get(url, auth=(user, tok))
+
+    # Print the user id
+    print(response.json().get('id'))
