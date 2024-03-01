945010aa3933eeb3ef551ae0bca7674b05167d61
oovaa <omarvx211@gmail.com>
2024-03-01 14:20:00 +0200

    task 4.2 at 01-14

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# Write a Python script that fetches
-# https://alx-intranet.hbtn.io/status
+"""# Write a Python script that fetches
+# https://alx-intranet.hbtn.io/status"""
 import requests
 
 if __name__ == '__main__':
