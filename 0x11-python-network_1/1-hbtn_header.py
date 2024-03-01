919cdfe7b9749bf2f9d1cc4d7f31c6848569a5d7
oovaa <omarvx211@gmail.com>
2024-03-01 14:13:27 +0200

    task 1.2 at 01-14

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-# Write a Python script that takes in a URL, sends a
-# request to the URL and displays the value of the X-Request-Id
-# variable found in the header of the response.
+"""Write a Python script that takes in a URL, sends a
+request to the URL and displays the value of the X-Request-Id
+variable found in the header of the response.
 
+"""
+if __name__ == '__main__':
 
-import urllib.request
-import sys
+    import urllib.request
+    import sys
 
-url = sys.argv[1]
+    url = sys.argv[1]
 
-with urllib.request.urlopen(url) as response:
-    headers = response.info()
-    print(headers.get('X-Request-Id'))
+    with urllib.request.urlopen(url) as response:
+        headers = response.info()
+        print(headers.get('X-Request-Id'))
