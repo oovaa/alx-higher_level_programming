621f5edaceb6c353a9da5f984ca0443554c3c8f8
oovaa <omarvx211@gmail.com>
2024-03-01 14:46:59 +0200

    8 shebang at 01-14

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-
+"""Write a Python script that takes your GitHub credentials 
+(username and password) and uses the GitHub API to display your id
+"""
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """# Write a Python script that fetches
 # https://alx-intranet.hbtn.io/status
 """
