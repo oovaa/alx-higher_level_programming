d24a86df80325d407ae4552f12ea1bfdb7e168a2
oovaa <omarvx211@gmail.com>
2024-03-01 19:37:45 +0200

    task 10.4 add indexerror handel at 01-19

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -20,8 +20,10 @@ if __name__ == '__main__':
 
     response = requests.get(url)
     commits = response.json()
-
-    for i in range(10):
-        print("{}: {}".format(
-            commits[i].get("sha"),
-            commits[i].get("commit").get("author").get("name")))
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
