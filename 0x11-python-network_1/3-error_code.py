1aba2a70bd07cdfff25b7e32f3f56613201f28e2
oovaa <omarvx211@gmail.com>
2024-03-01 14:17:08 +0200

    task 3.2 at 01-14

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-# Write a Python script that takes in a URL,
+"""# Write a Python script that takes in a URL,
 # sends a request to the URL and displays the body
-# of the response (decoded in utf-8).
-import sys
-import urllib.request
-from urllib.error import HTTPError
+# of the response (decoded in utf-8)."""
 
 if __name__ == '__main__':
+    import sys
+    import urllib.request
+    from urllib.error import HTTPError
+
     try:
         with urllib.request.urlopen(sys.argv[1]) as res:
             body_content = res.read()
